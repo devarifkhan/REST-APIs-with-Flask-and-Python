2009476825028b1aa5ebef9f3611e6c453680650
devarifkhan <arifcse209@gmail.com>
2024-06-11 16:37:26 +0600

    authentication with jwt extended

--- schemas.py
+++ schemas.py
@@ -20,13 +20,12 @@ class PlainTagSchema(Schema):
 class ItemSchema(PlainItemSchema):
     store_id = fields.Int(required=True, load_only=True)
     store = fields.Nested(PlainStoreSchema(), dump_only=True)
-    tags = fields.list(fields.Nested(PlainTagSchema(), dump_only=True))
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
 
 
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
-    store_id = fields.Int()
 
 
 class StoreSchema(PlainStoreSchema):
@@ -35,9 +34,9 @@ class StoreSchema(PlainStoreSchema):
 
 
 class TagSchema(PlainTagSchema):
-    store_id = fields.Int(dump_only=True)
-    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+    store_id = fields.Int(load_only=True)
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
 
 
 class TagAndItemSchema(Schema):
